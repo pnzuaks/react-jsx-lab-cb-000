e73b439f1cb395a858a1e14f9054d904c6f4f09b
Patricia Nykaza <pnykaza@calstatela.edu>
2019-02-06 23:12:53 +0000

    Automatically backed up by Learn

---
+++ src/components/Webpage.js
@@ -0,0 +1,13 @@
+import React, { Component } from 'react';
+
+export default class Webpage extends Component {
+  render() {
+    return (
+      <form>
+        <input type="text"> </>
+        <input type="password"> </>
+        <button type="submit"></button>
+      </form>
+    )
+  }
+}
